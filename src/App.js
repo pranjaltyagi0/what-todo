fa0c5af07a7904234979059dbd84844dedfad446
Pranjal Tyagi <pranjaltyagi@gmail.com>
2024-02-20 08:24:53 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import "./App.css";
-import "./components/card";
+import "./components/popup";
 import { useState } from "react";
-import Card from "./components/card";
+import Popup from "./components/popup";
 function App() {
   const [items, setitems] = useState([]);
   const [iscardVisible, setiscardVisisble] = useState(false);
   function newCardHandler() {
     setiscardVisisble(!iscardVisible);
   }
-  const [textContent,settextContent] = useState("");
-  console.log(textContent)
   return (
     <div className="Outside-Conatiner">
       <div className="Title">
         <h1>what-todo</h1>
       </div>
-      <div className="newCardButton">
+      <div className="newPopupButton">
         <button onClick={newCardHandler}>+</button>
       </div>
-      {iscardVisible && <Card  onwrite={settextContent}/> }
+      {/* {iscardVisible && <Popup  /> } */}
+      <Popup />
     </div>
   );
 }
---
+++ src/components/popup.js
@@ -0,0 +1,13 @@
+import "./popup.css"
+
+function Popup(){
+    return(
+        <div className="l1containerExternal">
+        <div className="overlay">
+            <input type="text" />
+        </div>
+        </div>
+    )
+
+}
+export default Popup;
